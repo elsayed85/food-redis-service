4843d4177afece75f3b1093602944292d60163fc
Elsayed Kamal <sayed.kamal@waseet.net>
2024-11-10 08:12:29 +0200

    “wip”

--- src/Services/StockService/DTO/ProductIngredientData.php
+++ src/Services/StockService/DTO/ProductIngredientData.php
@@ -5,7 +5,7 @@ namespace Elsayed85\LmsRedis\Services\StockService\DTO;
 class ProductIngredientData
 {
     public function __construct(
-        public readonly int $product_id,
+        public readonly int $order_product_id,
         public readonly string $ingredient_id,
         public readonly int $quantity,
         public readonly string $status
@@ -14,7 +14,7 @@ class ProductIngredientData
     public static function fromArray(array $data): self
     {
         return new self(
-            product_id: $data['product_id'],
+            order_product_id: $data['order_product_id'],
             ingredient_id : $data['ingredient_id'],
             quantity : $data['quantity'],
             status : $data['status']
