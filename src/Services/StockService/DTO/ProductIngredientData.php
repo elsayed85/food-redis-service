0b4ef83b71eba325c106b07266f7ad10a4f6ddbe
Elsayed Kamal <sayed.kamal@waseet.net>
2024-11-10 14:08:35 +0200

    “wip”

--- src/Services/StockService/DTO/ProductIngredientData.php
+++ src/Services/StockService/DTO/ProductIngredientData.php
@@ -6,7 +6,11 @@ class ProductIngredientData
 {
     public function __construct(
         public readonly int $order_product_id,
+        public readonly int $product_id,
+
+        public readonly int $product_ingredient_id,
         public readonly int $ingredient_id,
+
         public readonly int $quantity,
         public readonly float $remaining_percentage,
         public readonly string $status
@@ -16,7 +20,11 @@ class ProductIngredientData
     {
         return new self(
             order_product_id: $data['order_product_id'],
+            product_id : $data['product_id'],
+
+            product_ingredient_id : $data['product_ingredient_id'],
             ingredient_id : $data['ingredient_id'],
+            
             quantity : $data['quantity'],
             remaining_percentage : $data['remaining_percentage'],
             status : $data['status']
